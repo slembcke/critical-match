6c51dfb61e930f33a6fe36b8010007fdd286e3ad
slembcke <slembcke@gmail.com>
2018-06-25 22:02:09 -0500

    Improved player sprite function.

--- src/sprites.s
+++ src/sprites.s
@@ -11,118 +11,138 @@
 
 .import METATILE0, METATILE1, METATILE2, METATILE3, METATILE4
 
+CHR0 = $A0
 OBJ_COUNT = 6
 FRAME_COUNT = 14
-FRAME_SIZE = 4*OBJ_COUNT
 ROW_STRIDE = FRAME_COUNT*2
 
 .macro player_right_metasprite n
 	.scope
-		attr = $00
 		chr = $A0 + 2*n
-		.byte 248, 231, chr+0 + 0*ROW_STRIDE, attr
-		.byte   0, 231, chr+1 + 0*ROW_STRIDE, attr
-		.byte 248, 239, chr+0 + 1*ROW_STRIDE, attr
-		.byte   0, 239, chr+1 + 1*ROW_STRIDE, attr
-		.byte 248, 247, chr+0 + 2*ROW_STRIDE, attr
-		.byte   0, 247, chr+1 + 2*ROW_STRIDE, attr
+		.byte chr+0 + 0*ROW_STRIDE
+		.byte chr+1 + 0*ROW_STRIDE
+		.byte chr+0 + 1*ROW_STRIDE
+		.byte chr+1 + 1*ROW_STRIDE
+		.byte chr+0 + 2*ROW_STRIDE
+		.byte chr+1 + 2*ROW_STRIDE
 	.endscope
 .endmacro
 
 .macro player_left_metasprite n
 	.scope
-		attr = $40
 		chr = $A0 + 2*n
-		.byte 248, 231, chr+1 + 0*ROW_STRIDE, attr
-		.byte   0, 231, chr+0 + 0*ROW_STRIDE, attr
-		.byte 248, 239, chr+1 + 1*ROW_STRIDE, attr
-		.byte   0, 239, chr+0 + 1*ROW_STRIDE, attr
-		.byte 248, 247, chr+1 + 2*ROW_STRIDE, attr
-		.byte   0, 247, chr+0 + 2*ROW_STRIDE, attr
+		.byte chr+1 + 0*ROW_STRIDE
+		.byte chr+0 + 0*ROW_STRIDE
+		.byte chr+1 + 1*ROW_STRIDE
+		.byte chr+0 + 1*ROW_STRIDE
+		.byte chr+1 + 2*ROW_STRIDE
+		.byte chr+0 + 2*ROW_STRIDE
 	.endscope
 .endmacro
 
 .rodata
 
-player_frame_data:
+PLAYER_FRAME_DATA:
 	.repeat FRAME_COUNT, i
 		player_left_metasprite i
 		player_right_metasprite i
 	.endrepeat
 
-player_frames:
+PLAYER_FRAME_ADDRS:
 	.repeat 2*FRAME_COUNT, i
-		.addr player_frame_data + i*FRAME_SIZE
+		.addr PLAYER_FRAME_DATA + i*OBJ_COUNT
 	.endrepeat
 
 .zeropage
 
-sprite_x: .byte 0
-sprite_y: .byte 0
-sprite_pal: .byte 0
+sprite_x = tmp1
+sprite_y = tmp2
+sprite_pal = tmp3
 
 .code
 
 .export _player_sprite
 .proc _player_sprite ; u8 x, u8 y, u8 frame
-	; Load metsprite address.
+	ldx px_sprite_cursor
+	tay ; Save frame number.
+	
+	; Lowest bit of frame number is left/right.
+	eor #$FF
+	ror a
+	ror a
+	ror a
+	and #$40
+	ora #2 ; Set the palette bits.
+	
+	; Set attributes.
+	sta OAM+ 0+2, x
+	sta OAM+ 4+2, x
+	sta OAM+ 8+2, x
+	sta OAM+12+2, x
+	sta OAM+16+2, x
+	sta OAM+20+2, x
+	
+	; Fetch frame address.
+	tya ; Restore frame number.
 	asl
-	tax
-	lda player_frames+0, x
+	tay
+	lda PLAYER_FRAME_ADDRS+0, y
 	sta ptr1+0
-	lda player_frames+1, x
+	lda PLAYER_FRAME_ADDRS+1, y
 	sta ptr1+1
 	
-	; Set x/y offsets.
+	; Set chr.
+	ldy #0
+	lda (ptr1), y
+	sta OAM+ 0+1, x
+	iny
+	lda (ptr1), y
+	sta OAM+ 4+1, x
+	iny
+	lda (ptr1), y
+	sta OAM+ 8+1, x
+	iny
+	lda (ptr1), y
+	sta OAM+12+1, x
+	iny
+	lda (ptr1), y
+	sta OAM+16+1, x
+	iny
+	lda (ptr1), y
+	sta OAM+20+1, x
+	
+	; Set x-values.
 	ldy #1
 	lda (sp), y
-	sta sprite_x
-	dey
-	lda (sp), y
-	sta sprite_y
-	lda #2 ; TODO
-	sta sprite_pal
+	sta OAM+ 4+3, x
+	sta OAM+12+3, x
+	sta OAM+20+3, x
+	sub #8
+	sta OAM+ 0+3, x
+	sta OAM+ 8+3, x
+	sta OAM+16+3, x
+	add #8
 	
-	; TODO: Unroll.
-	lda #6
-	sta sreg
-	
-	ldx px_sprite_cursor
+	; Set y-values.
 	ldy #0
-	:	; x-pos
-		lda (ptr1), y
-		iny
-		clc
-		adc sprite_x
-		sta OAM+3, x
-		
-		; y-pos
-		lda (ptr1), y
-		iny
-		clc
-		adc sprite_y
-		sta OAM+0, x
-		
-		; chr
-		lda (ptr1), y
-		iny
-		sta OAM+1, x
-		
-		; attr
-		lda (ptr1), y
-		iny
-		ora sprite_pal
-		sta OAM+2, x
-		
-		inx
-		inx
-		inx
-		inx
-		
-		dec sreg
-		bne :-
+	lda (sp), y
+	sub #9
+	sta OAM+16+0, x
+	sta OAM+20+0, x
+	sub #8
+	sta OAM+ 8+0, x
+	sta OAM+12+0, x
+	sub #8
+	sta OAM+ 0+0, x
+	sta OAM+ 4+0, x
 	
-	stx px_sprite_cursor
+	; Increment sprite cursor.
+	ldx px_sprite_cursor
+	txa
+	clc
+	adc #(24)
+	sta px_sprite_cursor
+	
 	jmp incsp2
 .endproc
 

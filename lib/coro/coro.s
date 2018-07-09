fe42914475e023ccd39a83571e8f8fb805682c53
slembcke <slembcke@gmail.com>
2018-07-08 19:53:33 -0500

    Simplify coroutine switching.

--- lib/coro/coro.s
+++ lib/coro/coro.s
@@ -5,44 +5,22 @@
 
 .data
 
-CORO_RESUME_ADDR: .res 2
-CORO_YIELD_ADDR: .res 2
+CORO_IP: .res 2
 CORO_SP: .res 2
 CORO_STACK: .res 16
 CORO_STACK_END:
 
 .code
 
-.macro pull_address var
-	pla
-	sta var+0
-	pla
-	sta var+1
-.endmacro
-
-.macro push_address var
-	lda var+1
-	pha
-	lda var+0
-	pha
-.endmacro
-
-.macro swap_bytes var_a, var_b
-	lda var_a
-	ldx var_b
-	stx var_a
-	sta var_b
-.endmacro
-
 .export _coro_start
 .proc _coro_start ; coro_func func -> void
 	; Subtract 1 from the function address due to how jsr/ret work.
 	sub #1
-	sta CORO_RESUME_ADDR+0
+	sta CORO_IP+0
 	bcs :+
 		dex
 	:
-	stx CORO_RESUME_ADDR+1
+	stx CORO_IP+1
 	
 	lda #>(CORO_STACK_END)
 	sta CORO_SP+0
@@ -52,38 +30,44 @@ CORO_STACK_END:
 	rts
 .endproc
 
-.export _coro_resume
-.proc _coro_resume ; u8 value -> void
+.export _coro_yield = coro_switch
+.export _coro_resume = coro_switch
+.proc coro_switch ; u8 value -> void
 	; Save the resume value;
 	sta sreg
 	
-	swap_bytes sp+0, CORO_SP+0
-	swap_bytes sp+1, CORO_SP+1
+	; Swap stack pointers.
+	ldx sp+0
+	ldy sp+1
 	
-	; Swap out the return address.
-	pull_address CORO_YIELD_ADDR
-	push_address CORO_RESUME_ADDR
+	lda CORO_SP+0
+	sta sp+0
+	lda CORO_SP+1
+	sta sp+1
 	
-	; Return the resume address.
+	stx CORO_SP+0
+	sty CORO_SP+1
+	
+	; Stash the return address.
+	pla
+	tax
+	pla
+	tay
+	
+	; Push a new return address.
+	lda CORO_IP+1
+	pha
+	lda CORO_IP+0
+	pha
+	
+	; Save the old return address.
+	stx CORO_IP+0
+	sty CORO_IP+1
+	
+	; Load the return value.
 	lda sreg
 	ldx #0
-	rts
-.endproc
-
-.export _coro_yield
-.proc _coro_yield ; void -> u8
-	; Save the yield value.
-	sta sreg
 	
-	swap_bytes sp+0, CORO_SP+0
-	swap_bytes sp+1, CORO_SP+1
-	
-	; Swap out the return address.
-	pull_address CORO_RESUME_ADDR
-	push_address CORO_YIELD_ADDR
-	
-	; Return the yield value.
-	lda sreg
-	ldx #0
+	; Return to the new address.
 	rts
 .endproc

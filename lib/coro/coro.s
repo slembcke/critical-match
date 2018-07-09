23c284129210a4639ad634935d19b07b19d7efa4
slembcke <slembcke@gmail.com>
2018-07-08 21:10:30 -0500

    Catch returning functions.

--- lib/coro/coro.s
+++ lib/coro/coro.s
@@ -3,6 +3,10 @@
 .include "zeropage.inc"
 .macpack generic
 
+.import pusha, popa
+.import pushax
+.import _abort
+
 .data
 
 CORO_IP: .res 2
@@ -11,6 +15,28 @@ CORO_STACK: .res 16
 CORO_STACK_END:
 
 .code
+
+.proc coro_swap_stack
+	ldx sp+0
+	ldy sp+1
+	
+	lda CORO_SP+0
+	sta sp+0
+	lda CORO_SP+1
+	sta sp+1
+	
+	stx CORO_SP+0
+	sty CORO_SP+1
+	
+	rts
+.endproc
+
+.proc coro_ret_sreg
+	lda sreg+0
+	ldx sreg+1
+	
+	rts
+.endproc
 
 .export _coro_start
 .proc _coro_start ; coro_func func -> void
@@ -27,33 +53,34 @@ CORO_STACK_END:
 	lda #<(CORO_STACK_END)
 	sta CORO_SP+1
 	
+	jsr coro_swap_stack
+	lda #>(coro_catch - 1)
+	ldx #<(coro_catch - 1)
+	jsr pushax
+	
+	jsr coro_swap_stack
+	
 	rts
 .endproc
 
-.export _coro_yield = coro_switch
-.export _coro_resume = coro_switch
-.proc coro_switch ; u8 value -> void
+.export _coro_resume
+.proc _coro_resume ; u8 value -> void
 	; Save the resume value;
 	sta sreg+0
 	stx sreg+1
 	
-	; Swap stack pointers.
-	ldx sp+0
-	ldy sp+1
-	
-	lda CORO_SP+0
-	sta sp+0
-	lda CORO_SP+1
-	sta sp+1
-	
-	stx CORO_SP+0
-	sty CORO_SP+1
+	jsr coro_swap_stack
 	
 	; Stash the return address.
 	pla
-	tax
+	sta ptr1+0
 	pla
-	tay
+	sta ptr1+1
+	
+	jsr popa
+	pha
+	jsr popa
+	pha
 	
 	; Push a new return address.
 	lda CORO_IP+1
@@ -62,13 +89,64 @@ CORO_STACK_END:
 	pha
 	
 	; Save the old return address.
-	stx CORO_IP+0
-	sty CORO_IP+1
+	lda ptr1+0
+	sta CORO_IP+0
+	lda ptr1+1
+	sta CORO_IP+1
 	
-	; Load the return value.
-	lda sreg+0
-	ldx sreg+1
+	jmp coro_ret_sreg
+.endproc
+
+.export _coro_yield
+.proc _coro_yield ; u8 value -> void
+	; Save the resume value;
+	sta sreg+0
+	stx sreg+1
 	
-	; Return to the new address.
-	rts
+	; Stash the return address.
+	pla
+	sta ptr1+0
+	pla
+	sta ptr1+1
+	
+	pla
+	jsr pusha
+	pla
+	jsr pusha
+	
+	; Push a new return address.
+	lda CORO_IP+1
+	pha
+	lda CORO_IP+0
+	pha
+	
+	; Save the old return address.
+	lda ptr1+0
+	sta CORO_IP+0
+	lda ptr1+1
+	sta CORO_IP+1
+	
+	jsr coro_swap_stack
+	jmp coro_ret_sreg
+.endproc
+
+.proc coro_catch ; u8 value -> void
+	; Save the resume value;
+	sta sreg+0
+	stx sreg+1
+	
+	; Push a new return address.
+	lda CORO_IP+1
+	pha
+	lda CORO_IP+0
+	pha
+	
+	; Invalidate the resume address.
+	lda #<(_abort - 1)
+	sta CORO_IP+0
+	lda #>(_abort - 1)
+	sta CORO_IP+1
+	
+	jsr coro_swap_stack
+	jmp coro_ret_sreg
 .endproc
--- lib/coro/coro_test.c
+++ lib/coro/coro_test.c
@@ -10,29 +10,35 @@ void coro_start(coro_func func);
 uintptr_t coro_yield(uintptr_t);
 uintptr_t coro_resume(uintptr_t);
 
-uintptr_t func(uintptr_t n){
-	while(true){
-		printf("func() n: %d\n", n);
-		n = coro_yield(2*n);
-		
-		if(n == 10) abort();
-	}
-	
-	return 67;
+static void func2(uintptr_t n){
+	n = coro_yield(n);
+	n = coro_yield(n);
+	n = coro_yield(n);
+	n = coro_yield(n);
 }
+
+static uintptr_t func(uintptr_t n){
+	n = coro_yield(n);
+	n = coro_yield(n);
+	n = coro_yield(n);
+	n = coro_yield(n);
+	
+	return 0;
+}
+
+uint8_t stack1(void);
+uint8_t stack2(void);
 
 int main(void){
 	static uint8_t n;
+	
 	coro_start(func);
-	printf("%d\n", coro_resume(55));
 	
-	puts("main() resume.\n");
-	
-	while(true){
-		uint8_t value = coro_resume(n);
+	for(n = 1; n < 20; ++n){
+		uintptr_t value = coro_resume(n);
 		printf("main() n: %d, value: %d\n", n, value);
 		
-		++n;
+		if(value == 0) break;
 	}
 	
 	return EXIT_SUCCESS;

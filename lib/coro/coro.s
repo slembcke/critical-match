be898036255efce7603c0e562972028d801b7d9d
slembcke <slembcke@gmail.com>
2018-07-12 22:16:47 -0500

    Coros push return addresses onto the C stack.

--- lib/coro/coro.s
+++ lib/coro/coro.s
@@ -9,20 +9,20 @@
 .import incsp2
 
 ; What to call when resuming a coroutine that has finished.
-.import _exit
-CORO_ABORT = _exit
+.import _abort
+CORO_ABORT = _abort
 
-.zeropage
+.data
 
-CORO_BUFF_PTR: .res 2
+; CORO_BUFF_PTR: .res 2
+CORO_BUFF_PTR = regbank
 
-CORO_PC: .res 2 ; Yield/resume program counter - 1.
 CORO_SP: .res 2 ; Yield/resume stack pointer.
 CORO_S: .res 1 ; S register adjust value.
 
 .code
 
-.proc coro_swap_stack
+.proc coro_swap_sp
 	ldx sp+0
 	ldy sp+1
 	
@@ -37,34 +37,14 @@ CORO_S: .res 1 ; S register adjust value.
 	rts
 .endproc
 
-.proc coro_finish
-	; Save the old return address.
-	lda ptr1+0
-	sta CORO_PC+0
-	lda ptr1+1
-	sta CORO_PC+1
-	
-	; Load the return value.
-	lda sreg+0
-	ldx sreg+1
-	
-	rts
-.endproc
-
 .export _coro_init
 .proc _coro_init ; coro_func func, u8 *coro_buffer, size_t buffer_size -> void
-	; Stash buffer size.
-	sta sreg+0
-	stx sreg+1
+	func = ptr1
+	size = sreg
 	
-	; lda CORO_BUFF_PTR+0
-	; bne :+
-	; lda CORO_BUFF_PTR+1
-	; bne :+
-	; 	jsr coro_swap_stack
-	; 	jsr coro_save
-	; 	jsr coro_swap_stack
-	; :
+	; Stash buffer size.
+	sta size+0
+	stx size+1
 	
 	; Load the buffer pointer.
 	jsr popax
@@ -72,82 +52,36 @@ CORO_S: .res 1 ; S register adjust value.
 	stx CORO_BUFF_PTR+1
 	
 	; Store the end address into the stack pointer.
-	add sreg+0
+	add size+0
 	sta CORO_SP+0
 	txa
-	adc sreg+1
+	adc size+1
 	sta CORO_SP+1
 	
 	; Subtract 1 from the function address due to how jsr/ret work.
 	jsr popax
 	sub #1
-	sta CORO_PC+0
+	sta func+0
 	bcs :+
 		dex
 	:
-	stx CORO_PC+1
+	stx func+1
 	
-	; Push coro_catch - 1 onto the C stack in reverse byte order.
-	; When it's copied onto the CPU stack it will have the right byte order.
-	lda #2
-	sta CORO_S
+	jsr coro_swap_sp
 	
-	jsr coro_swap_stack
+	lda func+1
+	ldx func+0
+	jsr pushax
+	
 	lda #>(coro_catch - 1)
 	ldx #<(coro_catch - 1)
 	jsr pushax
-	jsr coro_save
-	jmp coro_swap_stack
-.endproc
-
-; .proc coro_save
-; 	; Push program counter.
-; 	lda CORO_PC+0
-; 	ldx CORO_PC+1
-; 	jsr pushax
 	
-; 	; Push stack offset.
-; 	lda CORO_S
-; 	jsr pusha
+	lda #2
+	sta CORO_S ; TODO
 	
-; 	; Save stack pointer.
-; 	ldy #0
-; 	lda CORO_SP+0
-; 	sta (CORO_BUFF_PTR), y
-; 	iny
-; 	lda CORO_SP+1
-; 	sta (CORO_BUFF_PTR), y
-	
-; 	rts
-; .endproc
-
-; .proc coro_restore ; void *coro_buffer
-; 	sta CORO_BUFF_PTR+0
-; 	stx CORO_BUFF_PTR+1
-	
-; 	; Restore stack pointer.
-; 	ldy #0
-; 	lda (CORO_BUFF_PTR), y
-; 	sta CORO_SP+0
-; 	iny
-; 	lda (CORO_BUFF_PTR), y
-; 	sta CORO_SP+1
-	
-; 	; Pop stack offset.
-; 	jsr popa
-; 	sta CORO_S
-	
-; 	; Pop program counter.
-; 	jsr popax
-; 	sta CORO_PC+0
-; 	stx CORO_PC+1
-; .endproc
-
-.export _coro_bind
-.proc _coro_bind ; u8 *coro_buffer
-	jsr coro_swap_stack
-	jsr coro_save
-	jsr coro_swap_stack
+	; Restore the stack.
+	jmp coro_swap_sp
 .endproc
 
 .export _coro_resume
@@ -156,13 +90,13 @@ CORO_S: .res 1 ; S register adjust value.
 	sta sreg+0
 	stx sreg+1
 	
-	; Stash the return address.
+	; Push the yield address to the caller's stack.
 	pla
-	sta ptr1+0
+	tax
 	pla
-	sta ptr1+1
+	jsr pushax
 	
-	jsr coro_swap_stack
+	jsr coro_swap_sp
 	
 	; Stash the stack register.
 	tsx
@@ -181,13 +115,17 @@ CORO_S: .res 1 ; S register adjust value.
 	; Save the old stack register value.
 	stx CORO_S
 	
-	; Push a new return address.
-	lda CORO_PC+1
+	; Pop the resume addres from the coroutine stack.
+	jsr popax
 	pha
-	lda CORO_PC+0
+	txa
 	pha
 	
-	jmp coro_finish
+	; Load the return value.
+	lda sreg+0
+	ldx sreg+1
+	
+	rts
 .endproc
 
 .export _coro_yield
@@ -196,11 +134,11 @@ CORO_S: .res 1 ; S register adjust value.
 	sta sreg+0
 	stx sreg+1
 	
-	; Stash the return address.
+	; Push the resume address onto the coroutine's stack.
 	pla
-	sta ptr1+0
+	tax
 	pla
-	sta ptr1+1
+	jsr pushax
 	
 	; Calculate stack offset. -(s - CORO_S)
 	tsx
@@ -221,26 +159,26 @@ CORO_S: .res 1 ; S register adjust value.
 			bne :-
 	@skip_copy:
 	
-	; Push a new return address.
-	lda CORO_PC+1
+	
+	jsr coro_swap_sp
+	
+	jsr popax
 	pha
-	lda CORO_PC+0
+	txa
 	pha
 	
-	jsr coro_swap_stack
-	jmp coro_finish
+	; Load the return value.
+	lda sreg+0
+	ldx sreg+1
+	
+	rts
 .endproc
 
 .proc coro_catch ; u16 -> u16
+	jmp _abort
 	; Save the resume value;
 	sta sreg+0
 	stx sreg+1
-	
-	; Push a new return address.
-	lda CORO_PC+1
-	pha
-	lda CORO_PC+0
-	pha
 	
 	; Invalidate the resume stack/address.
 	lda #0
@@ -251,6 +189,16 @@ CORO_S: .res 1 ; S register adjust value.
 	lda #>(CORO_ABORT - 1)
 	sta ptr1+1
 	
-	jsr coro_swap_stack
-	jmp coro_finish
+	jsr coro_swap_sp
+	
+	jsr popax
+	pha
+	txa
+	pha
+	
+	; Load the return value.
+	lda sreg+0
+	ldx sreg+1
+	
+	rts
 .endproc
--- lib/coro/coro_test.c
+++ lib/coro/coro_test.c
@@ -26,7 +26,10 @@ void func2(uintptr_t n){
 }
 
 uintptr_t func(uintptr_t n){
+	// log_stack("In func(). n: %d\n");
+	printf("In func(). n: %04X\n", n);
 	n = coro_yield(n);
+	printf("Back in func(). %n\n", n);
 	n = coro_yield(n);
 	
 	func2(n);
@@ -38,13 +41,25 @@ uintptr_t func(uintptr_t n){
 	return 0;
 }
 
-uint8_t buff[16];
+uint8_t buff[32];
 
 int main(void){
-	static uint8_t n;
+	static uintptr_t n;
 	
-	log_stack("before start");
+	printf("func: $%04X\n", func);
+	
+	// log_stack("before start");
 	coro_init(func, buff, sizeof(buff));
+	printf("Coroutine intialized.\n");
+	
+	printf("buff: ");
+	for(n = 0; n < sizeof(buff); ++n){
+		printf(" $%02X", buff[n]);
+	}
+	printf("\n");
+	
+	n = coro_resume(0xABCD);
+	printf("Back in main(). n: $%04X\n", n);
 	
 	for(n = 1; n < 20; ++n){
 		uintptr_t value;

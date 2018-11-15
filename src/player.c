54ab1ff94c252cc9a1b656d32355428045cb0eca
slembcke <slembcke@gmail.com>
2018-11-14 19:09:02 -0600

    Grapple fix.

--- src/player.c
+++ src/player.c
@@ -322,10 +322,10 @@ void player_draw_grapple(void){
 		
 		if(iy <= player.grapple_y) break;
 		
-		px_spr(ix, iy, 0x01, 0x1E);
+		px_spr(ix, iy, 0x01, 0x18);
 	}
 	// Draw the hook.
-	px_spr(ix, iy, 0x00, 0x1F);
+	px_spr(ix, iy, 0x00, 0x08);
 	
 	// Pull the block towards the player.
 	// TODO Not drawing code. Care?

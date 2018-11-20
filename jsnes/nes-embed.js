b4211913954e75692e05e555e2257926c5d8d148
slembcke <slembcke@gmail.com>
2018-11-19 23:00:12 -0600

    Basic audio support.

--- jsnes/nes-embed.js
+++ jsnes/nes-embed.js
@@ -2,20 +2,55 @@ var SCREEN_WIDTH = 256;
 var SCREEN_HEIGHT = 240;
 var FRAMEBUFFER_SIZE = SCREEN_WIDTH*SCREEN_HEIGHT;
 
-var ctx, image;
+var canvas_ctx, image;
 var framebuffer_u8, framebuffer_u32;
 
+var AUDIO_BUFFERING = 512;
+var SAMPLE_COUNT = 4*1024;
+var SAMPLE_MASK = SAMPLE_COUNT - 1;
+var audio_samples_L = new Float32Array(SAMPLE_COUNT);
+var audio_samples_R = new Float32Array(SAMPLE_COUNT);
+var audio_write_cursor = 0, audio_read_cursor = 0;
+
 var nes = new jsnes.NES({
-	onFrame: function(framebuffer){for(var i = 0; i < FRAMEBUFFER_SIZE; i++) framebuffer_u32[i] = 0xFF000000 | framebuffer[i];},
-	onAudioSample: function(left, right){}
+	onFrame: function(framebuffer){
+		for(var i = 0; i < FRAMEBUFFER_SIZE; i++) framebuffer_u32[i] = 0xFF000000 | framebuffer[i];
+	},
+	onAudioSample: function(l, r){
+		audio_samples_L[audio_write_cursor] = l;
+		audio_samples_R[audio_write_cursor] = r;
+		audio_write_cursor = (audio_write_cursor + 1) & SAMPLE_MASK;
+	},
 });
 
 function onAnimationFrame(){
 	window.requestAnimationFrame(onAnimationFrame);
 	
 	image.data.set(framebuffer_u8);
-	ctx.putImageData(image, 0, 0);
+	canvas_ctx.putImageData(image, 0, 0);
 	nes.frame();
+}
+
+function audio_remain(){
+	return (audio_write_cursor - audio_read_cursor) & SAMPLE_MASK;
+}
+
+function audio_callback(event){
+	var dst = event.outputBuffer;
+	var len = dst.length;
+	
+	// Attempt to avoid buffer underruns.
+	if(audio_remain() < AUDIO_BUFFERING) nes.frame();
+	
+	var dst_l = dst.getChannelData(0);
+	var dst_r = dst.getChannelData(1);
+	for(var i = 0; i < len; i++){
+		var src_idx = (audio_read_cursor + i) & SAMPLE_MASK;
+		dst_l[i] = audio_samples_L[src_idx];
+		dst_r[i] = audio_samples_R[src_idx];
+	}
+	
+	audio_read_cursor = (audio_read_cursor + len) & SAMPLE_MASK;
 }
 
 function keyboard(callback, event){
@@ -45,16 +80,21 @@ function keyboard(callback, event){
 
 function nes_init(canvas_id){
 	var canvas = document.getElementById(canvas_id);
-	ctx = canvas.getContext("2d");
-	image = ctx.getImageData(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	canvas_ctx = canvas.getContext("2d");
+	image = canvas_ctx.getImageData(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 	
-	ctx.fillStyle = "black";
-	ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+	canvas_ctx.fillStyle = "black";
+	canvas_ctx.fillRect(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 	
 	// Allocate framebuffer array.
-	var buf = new ArrayBuffer(image.data.length);
-	framebuffer_u8 = new Uint8ClampedArray(buf);
-	framebuffer_u32 = new Uint32Array(buf);
+	var buffer = new ArrayBuffer(image.data.length);
+	framebuffer_u8 = new Uint8ClampedArray(buffer);
+	framebuffer_u32 = new Uint32Array(buffer);
+	
+	var audio_ctx = new window.AudioContext();
+	var script_processor = audio_ctx.createScriptProcessor(AUDIO_BUFFERING, 0, 2);
+	script_processor.onaudioprocess = audio_callback;
+	script_processor.connect(audio_ctx.destination);
 }
 
 function nes_boot(rom_data){

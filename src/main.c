31d1e820a2516740714e5d9bb91e6258a6ba9150
slembcke <slembcke@gmail.com>
2018-11-20 20:38:37 -0600

    Remove old debug code.

--- src/main.c
+++ src/main.c
@@ -43,23 +43,6 @@ static GameState game_loop(void){
 	grid_init();
 	coins_init();
 	
-	// GRID[grid_block_idx(1, 1)] = BLOCK_GARBAGE;
-	// GRID[grid_block_idx(3, 1)] = BLOCK_KEY | BLOCK_COLOR_BLUE;
-	// GRID[grid_block_idx(4, 1)] = BLOCK_KEY | BLOCK_COLOR_RED;
-	// GRID[grid_block_idx(5, 1)] = BLOCK_KEY | BLOCK_COLOR_GREEN;
-	// GRID[grid_block_idx(6, 1)] = BLOCK_KEY | BLOCK_COLOR_PURPLE;
-	
-	// GRID[grid_block_idx(3, 4)] = BLOCK_CHEST | BLOCK_COLOR_BLUE;
-	// GRID[grid_block_idx(4, 4)] = BLOCK_CHEST | BLOCK_COLOR_RED;
-	// GRID[grid_block_idx(5, 4)] = BLOCK_CHEST | BLOCK_COLOR_GREEN;
-	// GRID[grid_block_idx(6, 4)] = BLOCK_CHEST | BLOCK_COLOR_PURPLE;
-	
-	// for(iy = 1; iy <= 10; ++iy) GRID[grid_block_idx(6, iy)] = BLOCK_GARBAGE;
-	// for(idx = 8; idx < GRID_BYTES - 8; ++idx){
-	// 	GRID[idx] = BLOCK_GARBAGE;
-	// }
-	// GRID[GRID_BYTES - 11] = BLOCK_EMPTY;
-	
 	px_inc(PX_INC1);
 	px_ppu_sync_off(); {
 		static const u8 PALETTE[] = {

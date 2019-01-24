d92a79523fbb41b23e28bc9fb4994bf6d1bd409e
slembcke <slembcke@gmail.com>
2019-01-23 22:45:46 -0600

    Fixing tmx2bin.py.

--- tools/tmx2bin.py
+++ tools/tmx2bin.py
@@ -11,7 +11,8 @@ data_u32 = base64.b64decode(data.text).strip()
 
 format = "{0}I".format(len(data_u32)/4)
 values = struct.unpack(format, data_u32)
-values = [e - 1 for e in values]
+# TODO generate attribute table here.
+values = [(e - 1) & 0xFF for e in values]
 
 # Append a blank attribute table if it's the size of a full name table.
 if len(values) == 960: values += [0]*64
